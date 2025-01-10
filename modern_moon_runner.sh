02ebfbed2b138bcf84c3607e9fc5023696a498b9
Daniel Talsky <danieltalsky@gmail.com>
2025-01-09 21:11:19 -0600

    Compile to go with the files

--- modern_moon_runner.sh
+++ modern_moon_runner.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 RUN_DIRECTORY="BDir2"
-# Chloe does it differently so let's just use something else
-SIMULATED_MACHINE="basil"
 
 ###############################################################################
 ### Script to repeatedly run moon-planet collision ratio simulations
@@ -16,49 +14,50 @@ output=1      # = log(years)
 step=0.5      # = days
 niter=1       # = number of iterations to run
 
-vers='mercury_TidesGas.for'
 user='no'     # use user-defined forces?
 
-nobj=6000     # number of ejected fragments
+nobj=100     # number of ejected fragments
 pl='J'        # planet to aim for
 
 ### Write files.in
-./writefiles.sh $RUN_DIRECTORY
+# ./writefiles.sh $RUN_DIRECTORY
 
-### Repeat simulation n times and record summary of collisions
-### Range for iterations
-if [ $machine = chloe ]; then
-    itrange=$(jot $niter 1)    # start at y, go up x-1 steps
-else
-    itrange=$(seq 1 $niter)    # go from x to y
-fi
+echo 'Running in '$RUN_DIRECTORY
+\rm $RUN_DIRECTORY/Out/*.dmp 2> /dev/null
+\rm $RUN_DIRECTORY/Out/*.out 2> /dev/null
 
-### Do iterations
-for j in $itrange; do
-    echo 'Iteration #' $j ' in '$RUN_DIRECTORY
-    \rm $RUN_DIRECTORY/Out/*.dmp
-    \rm $RUN_DIRECTORY/Out/*.out
+#### Randomize moon phases and rock positions
+# using only the mode previously called "gen"
+# which generates the files from scratch every time
 
-    #### Randomize moon phases and rock positions
-    # using only the mode previously called "gen"
-    # which generates the files from scratch every time
-    uv run python -c 'from merc_module.mercmodule import MercModule; MercModule.MakeBigRand("'$RUN_DIRECTORY'","'$j'")'
-    uv run python -c 'from merc_module.mercmodule import MercModule; MercModule.MakeSmall("'$RUN_DIRECTORY'","'$j'",'$nobj',"'$pl'",1.0e12,1.0e2)'
 
-    # Write param.in file
-    ./writeparam.sh $RUN_DIRECTORY $time $output $step $time $user
-    # Compile mercury
-    gfortran -std=legacy -w -o ${RUN_DIRECTORY}/Out/merc_${RUN_DIRECTORY} Files/$vers
+#uv run python -c 'from merc_module.mercmodule import MercModule; MercModule.MakeBigRand("'$RUN_DIRECTORY'","'$j'")'
+#uv run python -c 'from merc_module.mercmodule import MercModule; MercModule.MakeSmall("'$RUN_DIRECTORY'","'$j'",'$nobj',"'$pl'",1.0e12,1.0e2)'
 
-    #### Run mercury
-    cd $RUN_DIRECTORY/Out; ./merc_$RUN_DIRECTORY; cd ../..
 
-    ### Write collisions summary, copy good in coords
-    # using only the mode previously called "gen"
-    uv run python -c 'from merc_module.mercmodule import MercModule; MercModule.CopyInfo("'$RUN_DIRECTORY'","'$j'",True)'
 
-done    # j iterations
+# Write param.in file
+
+
+#./writeparam.sh $RUN_DIRECTORY $time $output $step $time $user
+
+
+
+# Compile modules
+cd Fortran03SourceFiles
+# gfortran -c mercury_constants.f03
+# gfortran -c swift_constants.f03
+gfortran -c mercury_TidesGas.f03
+gfortran mercury_TidesGas.o
+cd ..
+
+#### Run mercury
+# cd $RUN_DIRECTORY/Out; ./merc_MercuryTides90; cd ../..
+
+### Write collisions summary, copy good in coords
+# using only the mode previously called "gen"
+# uv run python -c 'from merc_module.mercmodule import MercModule; MercModule.CopyInfo("'$RUN_DIRECTORY'","'$j'",True)'
 
 # Write stop time for this directory:
 t2=$(date +%s)
-echo ""$RUN_DIRECTORY"    "$machine"    "$niter"    "$nobj"    "$user"    "$(echo "$t2 - $t1"|bc ) >> runtime.txt
+# echo ""$RUN_DIRECTORY"    "$machine"    "$niter"    "$nobj"    "$user"    "$(echo "$t2 - $t1"|bc ) >> runtime.txt
