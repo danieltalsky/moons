ece08a712c0236e4d7d157a51236c756d716a4cf
RJWorth <racheljworth@gmail.com>
2025-01-11 16:03:25 -0600

    un-comment remainder of run script

--- modern_moon_runner.sh
+++ modern_moon_runner.sh
@@ -46,21 +46,20 @@ for j in $itrange; do
     # uv run python -c 'from merc_module.mercmodule import MercModule; MercModule.MakeSmall("'$RUN_DIRECTORY'","'$j'",'$nobj',"'$pl'",1.0e12,1.0e2)'
     uv run python -c 'from merc_module.mercmodule import MercModule; MercModule.MakeSmallEjecta("'$RUN_DIRECTORY'","'$j'",'$nobj')'
 
-    done
-#     # Write param.in file
-#     ./writeparam.sh $RUN_DIRECTORY $time $output $step $time $user
-#     # Compile mercury
-#     gfortran -std=legacy -w -o ${RUN_DIRECTORY}/Out/merc_${RUN_DIRECTORY} Files/$vers
+    # Write param.in file
+    ./writeparam.sh $RUN_DIRECTORY $time $output $step $time $user
+    # Compile mercury
+    gfortran -std=legacy -w -o ${RUN_DIRECTORY}/Out/merc_${RUN_DIRECTORY} Files/$vers
 
-#     #### Run mercury
-#     cd $RUN_DIRECTORY/Out; ./merc_$RUN_DIRECTORY; cd ../..
+    #### Run mercury
+    cd $RUN_DIRECTORY/Out; ./merc_$RUN_DIRECTORY; cd ../..
 
-#     ### Write collisions summary, copy good in coords
-#     # using only the mode previously called "gen"
-#     uv run python -c 'from merc_module.mercmodule import MercModule; MercModule.CopyInfo("'$RUN_DIRECTORY'","'$j'",True)'
+    ### Write collisions summary, copy good in coords
+    # using only the mode previously called "gen"
+    uv run python -c 'from merc_module.mercmodule import MercModule; MercModule.CopyInfo("'$RUN_DIRECTORY'","'$j'",True)'
 
-# done    # j iterations
+done    # j iterations
 
-# # Write stop time for this directory:
-# t2=$(date +%s)
-# echo ""$RUN_DIRECTORY"    "$machine"    "$niter"    "$nobj"    "$user"    "$(echo "$t2 - $t1"|bc ) >> runtime.txt
+# Write stop time for this directory:
+t2=$(date +%s)
+echo ""$RUN_DIRECTORY"    "$machine"    "$niter"    "$nobj"    "$user"    "$(echo "$t2 - $t1"|bc ) >> runtime.txt
